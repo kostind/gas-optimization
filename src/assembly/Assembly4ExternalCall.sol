623a85a7f029e85873252d409b992e665dba3ab5
Dmytro Kostin <kostind@yahoo.com>
2026-05-08 18:05:00 +0200

    added block to PriceData

--- src/assembly/Assembly4ExternalCall.sol
+++ src/assembly/Assembly4ExternalCall.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { console } from "forge-std/console.sol";
+import {console} from "forge-std/console.sol";
 import "./Assembly4ExternalCall.sol";
 
 contract Assembly4ExternalCall {
@@ -12,33 +12,32 @@ contract Assembly4ExternalCall {
     }
 
     function processExpensive() external {
-        Factory.PriceData memory data = Factory.PriceData({
-            price: 13 * 10**18,
-            operatorId: 12345
-        });
+        Factory.PriceData memory data =
+            Factory.PriceData({price: 13 * 10 ** 18, operatorId: 12345, block: block.number});
 
         Factory(factory).setData(data);
     }
 
     function processOptimized() external {
-        Factory.PriceData memory data = Factory.PriceData({
-            price: 13 * 10**18,
-            operatorId: 12345
-        });
+        Factory.PriceData memory data =
+            Factory.PriceData({price: 13 * 10 ** 18, operatorId: 12345, block: block.number});
         address _factory = factory;
 
         bytes32 pointer;
         assembly {
-            pointer := sub(mload(0x40), 0x40)
+            //data size is 0x60
+            pointer := sub(mload(0x40), 0x60)
 
-            mcopy(add(pointer, 0x20), pointer, 0x40)
-            mstore(pointer, 0xc4bfa221) //setData((uint256,uint256))
+            //move data in memory for 1 slot (32 bytes)
+            mcopy(add(pointer, 0x20), pointer, 0x60)
+            mstore(pointer, 0x33af14d2) //setData((uint256,uint256,uint256))
 
             if iszero(extcodesize(_factory)) {
                 revert(0x00, 0x00)
             }
 
-            let success := call(gas(), _factory, 0x00, add(pointer, 28), 0x44, 0x00, 0x00)
+            //call data size if 0x64 (0x60 + 0x04)
+            let success := call(gas(), _factory, 0x00, add(pointer, 28), 0x64, 0x00, 0x00)
 
             if iszero(success) {
                 revert(0x00, 0x00)
@@ -53,6 +52,7 @@ contract Factory {
     struct PriceData {
         uint256 price;
         uint256 operatorId;
+        uint256 block;
     }
 
     function setData(PriceData memory _data) external {
@@ -62,5 +62,6 @@ contract Factory {
 
         console.log(data.price);
         console.log(data.operatorId);
+        console.log(data.block);
     }
 }
