fd68c14e6fb5ce2f8357f41625e4b94ab41bf57e
Dmytro Kostin <kostind@yahoo.com>
2026-05-08 11:10:42 +0200

    call using assembly

--- src/assembly/Assembly4ExternalCall.sol
+++ src/assembly/Assembly4ExternalCall.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 contract Assembly4ExternalCall {
-
     address public factory;
 
     constructor(address _factory) {
@@ -10,11 +9,27 @@ contract Assembly4ExternalCall {
     }
 
     function processExpensive() external {
-        Factory(factory).setData(123);
+        uint256 x = 123;
+        Factory(factory).setData(x);
     }
 
     function processOptimized() external {
-        Factory(factory).setData(123);
+        uint256 x = 123;
+        address _factory = factory;
+        assembly {
+            mstore(0x00, "0x5b4b73a9") //setData(uint256)
+            mstore(0x04, x)
+
+            if iszero(extcodesize(_factory)) {
+                revert(0x00, 0x00)
+            }
+
+            let success := call(gas(), _factory, 0x00, 0x00, 0x24, 0x00, 0x00)
+
+//            if iszero(success) {
+//                revert(0x00, 0x00)
+//            }
+        }
     }
 }
 
