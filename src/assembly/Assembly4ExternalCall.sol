6495ccf7f0d0d91a0beb7fa5c3259caf3d9602ea
Dmytro Kostin <kostind@yahoo.com>
2026-05-08 17:38:08 +0200

    Assembly4ExternalCall: use PriceData instead of uint256

--- src/assembly/Assembly4ExternalCall.sol
+++ src/assembly/Assembly4ExternalCall.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import { console } from "forge-std/console.sol";
+import "./Assembly4ExternalCall.sol";
+
 contract Assembly4ExternalCall {
     address public factory;
 
@@ -9,22 +12,33 @@ contract Assembly4ExternalCall {
     }
 
     function processExpensive() external {
-        uint256 x = 123;
-        Factory(factory).setData(x);
+        Factory.PriceData memory data = Factory.PriceData({
+            price: 13 * 10**18,
+            operatorId: 12345
+        });
+
+        Factory(factory).setData(data);
     }
 
     function processOptimized() external {
-        uint256 x = 123;
+        Factory.PriceData memory data = Factory.PriceData({
+            price: 13 * 10**18,
+            operatorId: 12345
+        });
         address _factory = factory;
+
+        bytes32 pointer;
         assembly {
-            mstore(0x00, hex"5b4b73a9") //setData(uint256)
-            mstore(0x04, x)
+            pointer := sub(mload(0x40), 0x40)
+
+            mcopy(add(pointer, 0x20), pointer, 0x40)
+            mstore(pointer, 0xc4bfa221) //setData((uint256,uint256))
 
             if iszero(extcodesize(_factory)) {
                 revert(0x00, 0x00)
             }
 
-            let success := call(gas(), _factory, 0x00, 0x00, 0x24, 0x00, 0x00)
+            let success := call(gas(), _factory, 0x00, add(pointer, 28), 0x44, 0x00, 0x00)
 
             if iszero(success) {
                 revert(0x00, 0x00)
@@ -34,9 +48,19 @@ contract Assembly4ExternalCall {
 }
 
 contract Factory {
-    uint256 public data;
+    PriceData public data;
 
-    function setData(uint256 _data) external {
+    struct PriceData {
+        uint256 price;
+        uint256 operatorId;
+    }
+
+    function setData(PriceData memory _data) external {
+        console.logBytes4(msg.sig);
+
         data = _data;
+
+        console.log(data.price);
+        console.log(data.operatorId);
     }
 }
